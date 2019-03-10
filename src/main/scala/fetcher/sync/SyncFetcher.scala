6fa0021ca8e221a5b6af59a6766bd4f793932bbf
Pavel Khamutou <p.khamutou@gmail.com>
2019-03-10 23:35:48 +0100

    add rec fetcher

--- src/main/scala/fetcher/sync/SyncFetcher.scala
+++ src/main/scala/fetcher/sync/SyncFetcher.scala
@@ -2,6 +2,8 @@ package fetcher.sync
 
 class SyncFetcher(metric: Metric) {
 
+  type Result[+A] = Either[String, A]
+
   /**
     * Fetches snapshots from provided URLs.
     * Each path is tried by each peer until it's successfully downloaded.
@@ -11,14 +13,14 @@ class SyncFetcher(metric: Metric) {
     * @param peers Clients that can download snapshots
     * @return Error message if at least one snapshot was not fetched or the list of snapshots
     */
-  def fetch(paths: List[String], peers: List[Peer]): Either[String, List[Snapshot]] =
-    paths.foldLeft[Either[String, List[Snapshot]]](Right(Nil)) {
+  def fetch(paths: List[String], peers: List[Peer]): Result[List[Snapshot]] =
+    paths.foldLeft[Result[List[Snapshot]]](Right(Nil)) {
       case (l @ Left(_), _) => l
       case (Right(xs), path) => fetchOne(path, peers).map(_ :: xs)
     }
 
-  private[sync] def fetchOne(path: String, peers: List[Peer]): Either[String, Snapshot] =
-    peers.foldLeft[Either[String, Snapshot]](Left(s"Failed downloading $path")) {
+  private[sync] def fetchOne(path: String, peers: List[Peer]): Result[Snapshot] =
+    peers.foldLeft[Result[Snapshot]](Left(s"Failed downloading $path")) {
       case (s @ Right(_), _) => s
       case (l @ Left(_), peer) => peer.fetch(path)
         .fold(
---
+++ src/main/scala/fetcher/sync/SyncRecFetcher.scala
@@ -0,0 +1,53 @@
+package fetcher.sync
+
+
+import scala.annotation.tailrec
+
+class SyncRecFetcher(metric: Metric) {
+
+  type Result[+A] = Either[String, A]
+
+  /**
+    * Fetches snapshots from provided URLs.
+    * Each path is tried by each peer until it's successfully downloaded.
+    * If all peers fail downloading at least one snapshot then return error message and interrupt further downloads.
+    *
+    * @param paths URLs to download snapshots from
+    * @param peers Clients that can download snapshots
+    * @return Error message if at least one snapshot was not fetched or the list of snapshots
+    */
+  def fetch(paths: List[String], peers: List[Peer]): Result[List[Snapshot]] = {
+
+    @tailrec def run(paths: List[String], acc: Result[List[Snapshot]]): Result[List[Snapshot]] = (acc, paths) match {
+      case (Right(snapshots), path :: xs) => fetchOne(path, peers) match {
+        case Left(error) => Left(error)
+        case Right(snapshot) => run(xs, Right(snapshot :: snapshots))
+      }
+
+      case (_, _) => acc
+    }
+
+    run(paths, Right(Nil))
+  }
+
+  private[sync] def fetchOne(path: String, peers: List[Peer]): Result[Snapshot] = {
+
+    @tailrec def run(peers: List[Peer], acc: Result[Snapshot]): Result[Snapshot] = peers match {
+      case Nil => acc
+      case peer :: xs => peer.fetch(path) match {
+        case scala.util.Success(s) =>
+          metric.increment(Metric.succeeded)
+          Right(s)
+
+        case _ =>
+          metric.increment(Metric.failed)
+          run(xs, acc)
+      }
+    }
+
+    run(peers, Left(s"Failed downloading $path"))
+  }
+
+}
+
+
